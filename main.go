8a49f838332ffd5ab9c4c2ab2e9a69f313fbb6a5
Luke McCrone <luke.r.mccrone@gmail.com>
2020-06-16 00:09:19 -0600

    initial db stuff

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+    "database/sql"
     "fmt"
     "time"
+
+    _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
     now := time.Now()
     fmt.Printf("Date:  %s\n", now.Format("Jan 2, 2006"))
+    createDatabase()
+}
+
+// TODO: fix go get/install for this
+func createDatabase() {
+    db, err := sql.Open("mysql", "admin:admin@tcp(127.0.0.1:3306)/")
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    defer db.Close()
+
+    dbs, err := db.Query("SHOW DATABASES")
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    var dbName string
+    for dbs.Next() {
+        dbs.Scan(&dbName)
+        fmt.Println(dbName)
+    }
 }
